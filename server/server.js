5679262adae961aa17c722d398d49badd932057d
anacollado <34594754+anacollado@users.noreply.github.com>
2019-07-03 12:57:39 -0500

    assemble data from api

--- server/server.js
+++ server/server.js
@@ -3,32 +3,77 @@ const axios = require('axios');
 const app = express();
 const port = 3000;
 const dummyData = [
-  {id: '1', name: 'Peever Beever', totalValue: 40010, location: 'Chicago IL, USA', deals: 1, tags: ['customer', 'canada']},
-  {id: '2', name: 'Hankus Pankus', totalValue: 80055, location: 'Albany NY, USA', deals: 4, tags: ['customer', 'america']},
-  {id: '3', name: 'Penny Lenny', totalValue: 70330, location: 'London, UK', deals: 2, tags: ['retailer', 'europe']}
+  { id: '1', name: 'Peever Beever', totalValue: 40010, location: 'Chicago IL, USA', deals: 1, tags: ['customer', 'canada'] },
+  { id: '2', name: 'Hankus Pankus', totalValue: 80055, location: 'Albany NY, USA', deals: 4, tags: ['customer', 'america'] },
+  { id: '3', name: 'Penny Lenny', totalValue: 70330, location: 'London, UK', deals: 2, tags: ['retailer', 'europe'] }
 ];
 
 
 
 app.use('/', express.static(__dirname + '/../client/dist'));
 
-const getAllContacts =  () => {
-  return Promise.resolve(dummyData);
+
+
+const getAllContacts = () => {
+  return axios('https://lamppoststudios.api-us1.com/api/3/contacts?include=geoIps.geoAddress,contactTags.tag,deals,contactData', {
+    method: 'GET',
+    headers: {
+      'Api-Token' : '0f7e5c9167768f6bb0a6e09e335ce464da7cb5e7008b989f0057266c26342424a4d8d3e5'
+    }
+  })
+    .then(req => req.data);
 }
 
-// const getAllContacts = () => {
-//   return axios('https://lamppoststudios.api-us1.com/api/3/contacts?include=geoIps,contactTags,contactDeals', {
-//     method: 'GET',
-//     headers: {
-//       'Api-Token' : '0f7e5c9167768f6bb0a6e09e335ce464da7cb5e7008b989f0057266c26342424a4d8d3e5'
-//     }
-//   })
-//     .then(req => req.data);
-// }
+const getDealTotalsByContactId = deals => {
+  return deals.reduce((acc, currentDeal) => {
+    if (acc.hasOwnProperty(currentDeal.contact)) {
+      acc[currentDeal.contact] += parseInt(currentDeal.value);
+    } else {
+      acc[currentDeal.contact] = parseInt(currentDeal.value);
+    }
+    return acc;
+  }, {});
+}
+
+const getContactFields = (data) => {
+  const dealTotalsByContactId = getDealTotalsByContactId(data.deals);
+
+  const tagsById = data.tags.reduce((acc, curr) => {
+    acc[curr.id] = curr.tag;
+    return acc;
+  }, {});
+
+  const contactTagsById = data.contactTags.reduce((acc, curr) => {
+    acc[curr.id] = tagsById[curr.tag];
+    return acc;
+  }, {});
+
+  const locationStrByContactId = data.contactData.reduce((acc, curr) => {
+    acc[curr.contact] = [curr.geoCity, curr.geoState, curr.geo_country].filter(x => !!x).join(', ');;
+    return acc;
+  }, {});
+
+  return data.contacts.map(contact => {
+    return {
+      tags: contact.contactTags.map(ctId => {
+        const tagName = contactTagsById[ctId];
+        return tagName;
+      }),
+      locationStr: locationStrByContactId[contact.id] || '',
+      totalValue: dealTotalsByContactId[contact.id],
+      firstName: contact.firstName,
+      lastName: contact.lastName,
+      deals: contact.deals.length,
+      id: contact.id
+    }
+  }
+  )
+}
+
 
 app.get('/contacts', function (req, res) {
   getAllContacts()
-  .then(data => res.send(data).status(200));
+    .then(data => res.send(getContactFields(data)).status(200));
 })
 
 
